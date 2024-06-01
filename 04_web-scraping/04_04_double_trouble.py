6d6c152515e1b9e47e7f7fa9b3af29e382c7f7c5
Seityl <153787001+Seityl@users.noreply.github.com>
2024-06-01 13:11:05 -0400

    dunno
    fr tho

--- 04_web-scraping/04_04_double_trouble.py
+++ 04_web-scraping/04_04_double_trouble.py
@@ -4,3 +4,22 @@
 # data that you receive from both of them.
 # E.g.: You could use the Ghibli API to find all ghosts from their films, and
 #       create an opposing team of Ghost Pokémon from the Poke API.
+
+import requests
+
+def get_all_characters():
+    response = requests.get('https://ghibliapi.vercel.app/species')
+    characters = response.json()
+    return characters
+
+def find_ghosts():
+    characters = get_all_characters()
+    ghosts = []
+    for character in characters:
+        if 'ghost' in character['name'].lower() or 'cat' in character['species'].lower():
+            ghosts.append(character)
+    return ghosts
+
+ghosts = find_ghosts()
+for ghost in ghosts:
+    print(ghost['name'])
