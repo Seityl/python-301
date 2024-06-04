b7df9f2458debdcfd79afcbd8ab25c8f746e1b65
Seityl <153787001+Seityl@users.noreply.github.com>
2024-06-04 16:26:35 -0400

    more testing
    success

--- 06_testing/06_02_mymath-tester/test_mymath.py
+++ 06_testing/06_02_mymath-tester/test_mymath.py
@@ -4,3 +4,14 @@
 # 1. Check for correct results by providing example input.
 # 2. Check that a `CustomZeroDivisionError` gets raised correctly.
 
+import unittest
+import mymath
+from mymath import CustomZeroDivsionError
+
+class TestMyMath(unittest.TestCase):
+    def test_subtract_divide(self):
+        self.assertEqual(mymath.subtract_divide(0, 0, 0), 1)
+    def text_zero_divison_error(self):
+        self.assertRaises(CustomZeroDivsionError)
+if __name__ == "__main__":
+    unittest.main()
