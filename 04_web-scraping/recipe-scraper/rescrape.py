6e0b5d0614977095064bbf1f103b1f0913027ef4
Seityl <153787001+Seityl@users.noreply.github.com>
2024-06-06 15:26:58 -0400

    progress

--- 04_web-scraping/recipe-scraper/rescrape.py
+++ 04_web-scraping/recipe-scraper/rescrape.py
@@ -8,4 +8,38 @@
 # into the code base, to get some additional practice in working with your
 # custom Python classes.
 
-URL = "https://codingnomads.github.io/recipes"
+import requests
+from bs4 import BeautifulSoup
+
+# Take name of ingredients as input and return list of ingredients from user
+def getInput(prompt):
+    ingredients = input(prompt + ' (separate values with \", \"):')
+    if ingredients:
+        ingredients = ingredients.split(', ')
+        print("Success: Got ingredients")
+        return ingredients
+print(getInput("Enter Ingredients"))
+
+# Fetch content from the recipie collection
+def fetchRecipieInformation():
+    # Recipie API link
+    url = "https://codingnomads.github.io/recipes"
+    # Send get request to API
+    response = requests.get(url)
+    response.raise_for_status()
+    print("Success: Fetched content")
+    return response.content
+
+# Parse and return recipies
+def getRecipies(soup):
+    page = BeautifulSoup(soup, 'html.parser')
+    text = page.find_all('a')
+    recipies = []
+    for r in text:
+        recipies.append(r.get_text())
+    allRecipies = '\n'.join(recipies)
+    return allRecipies
+
+# Search through the receipts to find one that includes the provided ingredients
+
+print(getRecipies(fetchRecipieInformation()))
