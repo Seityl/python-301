3f18030252de617436a8185279e5396a9107920a
Seityl <153787001+Seityl@users.noreply.github.com>
2024-06-11 16:45:37 -0400

    Busy ahh day
    damn

--- 07_decorators/07_01_quote_wrap.py
+++ 07_decorators/07_01_quote_wrap.py
@@ -1,3 +1,10 @@
 # Write a decorator function that wraps text output into quotes, e.g.:
 # Hello world! ----> "Hello World!"
 # You can use it to create quotes from text output.
+
+def decorate(text: str):
+    result = []
+    result = '\"' + text + '\"'
+    return result
+
+print(decorate('Hello World!'))
--- 07_decorators/07_02_censorship.py
+++ 07_decorators/07_02_censorship.py
@@ -4,3 +4,34 @@
 #    "I bumped my toe! Shoot!"
 # Would, after decorating it with `@censor()`, return:
 #    "I bumped my toe! S****!"
+
+# TODO: FINISH PLEASE
+
+def decorator(function):
+    def censor(*args):
+        print("\nBefore Censoring:\n" + "-" * 25)
+        result = function(*args)
+        print(result)
+        print("\nAfter Censoring:\n" + "-" * 25)
+        sentences = result.split("!")
+        censoredSentences = []
+        for sentence in sentences:
+            words = sentence.split(" ")
+            censoredWords = []
+            for word in words:
+                if "!" in word:
+                    word = word.replace("!", "")
+                if word.lower() == "shoot":
+                    censoredWords.append("S****")
+                else:
+                    censoredWords.append(word)
+            censoredSentence = " ".join(censoredWords)
+            censoredSentences.append(censoredSentence + "!")
+        return " ".join(censoredSentences)
+    return censor
+
+@decorator
+def sentence(oWord):
+    return "I bumped my toe! " + oWord
+
+print(sentence('Shoot!'))
