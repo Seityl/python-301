6ead88ffe2b67a24066d979dbeacb6a94ab59e2e
Seityl <153787001+Seityl@users.noreply.github.com>
2024-06-25 16:54:18 -0400

    mhm

--- projects/pokemon_game.py
+++ projects/pokemon_game.py
@@ -11,22 +11,25 @@
 # - Display messages that explain who won or lost a battle
 # - If a Pokemon loses a battle, they lose some of their `hp`
 # - If you call the `feed()` method on a Pokemon, they regain some `hp`
+
 from enum import Enum
 import random
 
 class PrimaryType(Enum):
+    # Initializes primary types using enum
     WATER = "Water"
     FIRE = "Fire"
     GRASS = "Grass"
 
 class Pokemon:
-    # Initialize pokemone class with pokemon name, primary type, max hp & hp
+    # Initialize pokemon class with pokemon name, primary type, max hp & hp
     def __init__(self, name,  primaryType, maxHp, hp):
         print(f"Initializing Pokemon w/ Name: {name}, Primary Type: {primaryType}, Max HP: {maxHp}, HP: {hp}")
         
         self.name = name
-        if isinstance(primaryType, PrimaryType):
-            self.primaryType = primaryType
+        primaryType = primaryType.lower()
+        if primaryType == 'water' or primaryType == 'fire' or primaryType == 'grass':
+            self.primaryType = primaryType.capitalize()
         else:
             raise ValueError("Wrong Primary Type Provided. 'Water', 'Fire', 'Grass' Only.  ")
 
@@ -39,15 +42,13 @@ class Pokemon:
         return cls(name, randomType, maxHp, hp)
 
     def __repr__(self):
-        return f"Name: {self.name}, Primary Type: {self.primaryType.value}, Max HP: {self.maxHp}, HP: {self.hp}"
+        return f"Name: {self.name}, Primary Type: {self.primaryType}, Max HP: {self.maxHp}, HP: {self.hp}"
 
 try:
-    p1 = Pokemon("Jeriel", PrimaryType.WATER, 1000, 1000)
+    p1 = Pokemon("Jeriel", "Water", 1000, 1000)
     print(f"Pokemon Instance Created Successfully: {p1}")
 except ValueError as e:
     print("Error Occured During Creating Pokemon: ", e)
 
-randomPokemon = Pokemon.createRandom("Random Pokemon", 1200, 1200)
-print(randomPokemon)
 def battle():
     pass
